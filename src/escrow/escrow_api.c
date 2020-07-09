25d8066f421630330badd11b1af165a94cae5e1a
jospaeth <spaethj@in.tum.de>
2020-07-09 14:55:41 +0200

    include sss library checks, minor fixes

--- src/escrow/escrow_api.c
+++ src/escrow/escrow_api.c
@@ -1,1 +1,31 @@
+/*
+   This file is part of GNUnet.
+   Copyright (C) 2020 GNUnet e.V.
+
+   GNUnet is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   GNUnet is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   SPDX-License-Identifier: AGPL3.0-or-later
+ */
+
+/**
+ * @author Johannes Späth
+ * @file escrow/escrow_api.c
+ * 
+ * @brief api to interact with the escrow component
+ */
+
+
+
 // GNUNET_ESCROW_put(Ego, ...)
+// GNUNET_ESCROW_get(anchor, Ego, ...)
