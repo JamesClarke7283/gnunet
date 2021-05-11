07100e184d3ace651280fb76a2e414430d50b1b9
Elias Summermatter <elias.summermatter@seccom.ch>
2021-05-11 20:52:17 +0200

    Perftest

--- src/setu/perf_setu_api.c
+++ src/setu/perf_setu_api.c
@@ -404,7 +404,7 @@ run (void *cls,
                 "Running real set-reconciliation\n");
     //init_set1 ();
     // limit ~23800 element total
-    initRandomSets(460,500,500,32);
+    initRandomSets(480,500,500,32);
 }
 
 void perf_thread() {
