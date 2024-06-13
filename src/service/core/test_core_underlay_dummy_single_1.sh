0a686288c387d736408288aa1f44973137628cb2
ch3 <buenger@mytum.de>
2024-06-13 13:22:30 +0200

    cong: cleanup shell scripts

--- src/service/core/test_core_underlay_dummy_single_1.sh
+++ src/service/core/test_core_underlay_dummy_single_1.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-#strace -f -e trace=network,close,shutdown .libs/test_core_underlay_dummy_single > test_core_underlay_dummy_single_0.log 2>&1
-.libs/test_core_underlay_dummy_single > test_core_underlay_dummy_single_1.log 2>&1
+#TEST_PREFIX='strace -f -e trace=network,close,shutdown'
+TEST_PREFIX='valgrind --track-origins=yes'
+GNUNET_FORCE_LOG='core-underlay-dummy;;;;DEBUG/core;;;;DEBUG/;;;;INFO'
+$TEST_PREFIX .libs/test_core_underlay_dummy_single > test_core_underlay_dummy_single_1.log 2>&1
