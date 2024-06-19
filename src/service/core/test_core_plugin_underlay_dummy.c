a61eaea19cdf5901fd1d34d1a55c9c310dfd7547
ch3 <buenger@mytum.de>
2024-06-19 17:11:19 +0200

    cong: use plugin architecture of testing

---
+++ src/service/core/test_core_plugin_underlay_dummy.c
@@ -0,0 +1,231 @@
+/*
+      This file is part of GNUnet
+      Copyright (C) 2024 GNUnet e.V.
+
+      GNUnet is free software: you can redistribute it and/or modify it
+      under the terms of the GNU Affero General Public License as published
+      by the Free Software Foundation, either version 3 of the License,
+      or (at your option) any later version.
+
+      GNUnet is distributed in the hope that it will be useful, but
+      WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+      Affero General Public License for more details.
+
+      You should have received a copy of the GNU Affero General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     SPDX-License-Identifier: AGPL3.0-or-later
+ */
+
+/**
+ * @file src/service/core/test_core_plugin_underlay_dummy.c
+ * @brief a plugin to provide the API for running test cases.
+ * @author ch3
+ */
+#include "platform.h"
+#include "gnunet_util_lib.h"
+#include "gnunet_testing_lib.h"
+
+#include "gnunet_core_underlay_dummy.h"
+
+
+#define MTYPE 12345
+
+
+struct UnderlayDummyState
+{
+  struct GNUNET_CORE_UNDERLAY_DUMMY_Handle *h;
+  struct GNUNET_MQ_Handle *mq;
+} uds;
+
+
+struct GNUNET_UNDERLAY_DUMMY_Message
+{
+  struct GNUNET_MessageHeader header;
+  // The following will be used for debugging
+  uint64_t id; // id of the message
+  uint64_t batch; // first batch of that peer (for this test 0 or 1)
+  //uint64_t peer; // number of sending peer (for this test 0 or 1)
+};
+
+
+/**
+ * This function prepares an array with traits.
+ */
+static enum GNUNET_GenericReturnValue
+connect_traits (void *cls,
+                const void **ret,
+                const char *trait,
+                unsigned int index)
+{
+  struct UnderlayDummyState *uds = cls;
+  struct GNUNET_TESTING_Trait traits[] = {
+    GNUNET_CORE_make_trait_connect (uds->h),
+    GNUNET_TESTING_trait_end ()
+  };
+
+  return GNUNET_TESTING_get_trait (traits,
+                                   ret,
+                                   trait,
+                                   index);
+}
+
+
+static void
+handle_test (void *cls, const struct GNUNET_UNDERLAY_DUMMY_Message *msg)
+{
+  struct UnderlayDummyState *uds = cls;
+}
+
+
+void *notify_connect_cb (
+  void *cls,
+  uint32_t num_addresses,
+  const char *addresses[static num_addresses],
+  struct GNUNET_MQ_Handle *mq)
+{
+  struct UnderlayDummyState *uds = cls;
+
+  uds->mq = mq;
+
+  // GNUNET_TESTING_AsyncContext *ac
+  //GNUNET_TESTING_async_finish ();
+}
+
+
+void address_change_cb (void *cls,
+                        struct GNUNET_HashCode network_location_hash,
+                        uint64_t network_generation_id)
+{
+  struct UnderlayDummyState *uds = cls;
+}
+
+
+static void
+exec_connect_run (void *cls,
+                  struct GNUNET_TESTING_Interpreter *is)
+{
+  struct UnderlayDummyState *uds = cls;
+
+  struct GNUNET_MQ_MessageHandler handlers[] =
+  {
+    GNUNET_MQ_hd_fixed_size (test, MTYPE, struct GNUNET_UNDERLAY_DUMMY_Message, NULL),
+    GNUNET_MQ_handler_end ()
+  };
+
+  uds->h = GNUNET_CORE_UNDERLAY_DUMMY_connect (NULL, // cfg
+                                               handlers,
+                                               uds, // cls
+                                               notify_connect_cb,
+                                               NULL, // nd
+                                               address_change_cb);
+
+}
+
+
+static void
+exec_connect_cleanup (void *cls)
+{
+  struct UnderlayDummyState *uds = cls;
+
+  GNUNET_assert (NULL != uds->h);
+  GNUNET_CORE_UNDERLAY_DUMMY_disconnect (uds->h);
+}
+
+
+const struct GNUNET_TESTING_Command
+GNUNET_CORE_cmd_connect (
+  const char *label,
+  enum GNUNET_OS_ProcessStatusType expected_type,
+  unsigned long int expected_exit_code,
+  struct UnderlayDummyState *uds)
+{
+  return GNUNET_TESTING_command_new (
+      uds, // state
+      label,
+      &exec_connect_run,
+      &exec_connect_cleanup,
+      &connect_traits);
+}
+
+
+static void
+exec_send_run (void *cls,
+               struct GNUNET_TESTING_Interpreter *is)
+{
+  struct UnderlayDummyState *uds = cls;
+  struct GNUNET_MQ_Envelope *env;
+  struct GNUNET_UNDERLAY_DUMMY_Message *msg;
+
+  GNUNET_assert (NULL != uds->mq);
+  env = GNUNET_MQ_msg (msg, MTYPE); // TODO usually we wanted to keep the
+                                    // envelopes to potentially cancel the
+                                    // message
+  msg->id = GNUNET_htonll (0); // i
+  msg->batch = GNUNET_htonll (0); // dc->num_open_connections - 1
+  GNUNET_MQ_send (uds->mq, env);
+}
+
+
+static void
+exec_send_cleanup (void *cls)
+{
+  struct UnderlayDummyState *uds = cls;
+
+}
+
+
+const struct GNUNET_TESTING_Command
+GNUNET_CORE_cmd_send (
+  const char *label,
+  enum GNUNET_OS_ProcessStatusType expected_type,
+  unsigned long int expected_exit_code,
+  struct UnderlayDummyState *uds)
+{
+  return GNUNET_TESTING_command_new (
+      uds, // state
+      label,
+      &exec_send_run,
+      &exec_send_cleanup,
+      &connect_traits);
+}
+
+
+GNUNET_TESTING_MAKE_PLUGIN (
+  libgnunet_test_core,
+  underlay_dummy,
+    //GNUNET_TESTING_cmd_barrier_create ("barrier-connected", 5),
+    GNUNET_TESTING_cmd_load_topology_from_file (
+      "load-topology",
+      "test_core_underlay_dummy_netjail_topo.conf"),
+    GNUNET_TESTING_cmd_netjail_start_helpers ("netjail-start-helpers",
+                                              "load-topology",
+                                              GNUNET_TIME_UNIT_MINUTES),
+    GNUNET_TESTING_cmd_netjail_setup ("netjail",
+                                      GNUNET_TESTING_NETJAIL_START_SCRIPT,
+                                      "load-topology"),
+    GNUNET_TESTING_cmd_make_unblocking (
+      GNUNET_CORE_cmd_connect ("connect",
+                               GNUNET_OS_PROCESS_EXITED,
+                               0,
+                               &uds)),
+    //GNUNET_TESTING_cmd_make_unblocking (
+    //  GNUNET_TESTING_cmd_barrier_reached ("barrier-connected-r",
+    //                                      "barrier-connected")),
+    //GNUNET_TESTING_cmd_finish ("barrier-connected-finish",
+    //                           "barrier-connected",
+    //                           GNUNET_TIME_UNIT_MINUTES),
+    //GNUNET_CORE_cmd_send ("send", GNUNET_OS_PROCESS_EXITED, 0, &uds),
+    GNUNET_TESTING_cmd_end ()
+  )
+
+
+// testing_core_cmd_connecting_peers.c takes as inspiration
+// FIXME: likely not ideally placed here, move to its own file
+GNUNET_CORE_SIMPLE_DUMMY_UNDERLAY_TRAITS (
+    GNUNET_TESTING_MAKE_IMPL_SIMPLE_TRAIT, GNUNET_CORE)
+
+
+/* end of src/service/core/test_core_plugin_underlay_dummy.c */
+
---
+++ src/service/core/test_core_underlay_dummy_testing.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec ../../../scripts/netjail/netjail_test_master.sh gnunet-testing-netjail-launcher test_core_underlay_dummy_netjail_topo.conf # --num-peers=2
